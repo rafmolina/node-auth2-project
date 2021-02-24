aecbb78578a1978065f1678f0f30696d8ca28019
Rafael Molina <seissaysseis@users.noreply.github.com>
2021-02-24 13:48:53 -0700

    migration and seed needed

--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ const helmet = require('helmet');
 const cors = require('cors');
 
 const usersRouter = require('./users/users-router');
+const authRouter = require('./auth/auth-router')
 
 
 const server = express();
@@ -12,6 +13,7 @@ server.use(express.json());
 server.use(cors());
 
 server.use('/api/users', usersRouter);
+server.use('/api/auth', authRouter);
 
 
 server.get('/', (req, res) => {
---
+++ config/secrets.js
@@ -0,0 +1,5 @@
+const jwtSecret = process.env.JWT_SECRET || "phantom menace is good"
+
+module.exports = {
+    jwtSecret
+}
